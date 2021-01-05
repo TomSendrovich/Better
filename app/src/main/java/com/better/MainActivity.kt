5c23d68d062cbb7a04894d909b6d880c7b1ff8f7
Guy Shoham <guyshoham28@gmail.com>
2021-01-05 19:45:50 +0200

    repository read from firestore

--- app/src/main/java/com/better/MainActivity.kt
+++ app/src/main/java/com/better/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.better
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.better.model.Repository
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Repository.demo()
     }
 }
--- app/src/main/java/com/better/model/Repository.kt
+++ app/src/main/java/com/better/model/Repository.kt
@@ -1,4 +1,25 @@
 package com.better.model
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 object Repository {
+    private val db: FirebaseFirestore = Firebase.firestore
+    private const val TAG = "Repository"
+
+
+    fun demo() {
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
 }
