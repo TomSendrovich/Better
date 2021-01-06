ccc2425b5cc192c4684cf9bf92afdc2fbbeffe5f
Guy Shoham <guyshoham28@gmail.com>
2021-01-06 12:52:10 +0200

    add SplashScreen

--- app/src/main/java/com/better/ui/MainActivity.kt
+++ app/src/main/java/com/better/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.better
+package com.better.ui
 
 import android.os.Bundle
 import android.view.Menu
@@ -10,6 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.better.R
 import com.better.model.Repository
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
---
+++ app/src/main/java/com/better/ui/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.better.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.better.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // set logo animation
+        val logo: ImageView = findViewById(R.id.SplashScreenImage)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        logo.startAnimation(animation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
